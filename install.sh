f28da8723914a7675e02e3b8a1935cd232b3556b
litchnox <95579699+litchnox@users.noreply.github.com>
2021-12-05 10:42:42 -0600

    install script
    toos to install

---
+++ install.sh
@@ -0,0 +1,7 @@
+#?/bin/bash 
+#xorg
+sudo apt install xserver-xorg-core xserver-xorg-video-qxl xserver-xorg-video-intel xserver-xorg-input-all xserver-xorg -y
+#audio
+sudo apt install  pulseaudio pavucontrol -y
+sudo apt install kitty rofi  build-essential dunst feh i3-gaps lm-sensors -y
+sudo apt install lm-sensors mousepad virt-manager libnotify-bin pcmanfm lxappearance polybar firefox-esr lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings -y
